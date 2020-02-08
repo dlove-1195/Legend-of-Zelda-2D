a1a2ba05bc705077bc4039d8cc21c48790bc294d
dlove-1195 <60125456+dlove-1195@users.noreply.github.com>
2020-02-08 14:56:47 -0500

    upload folder

---
+++ Sprite2/Isprite/ISprite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprite2
+{
+    public interface ISprite
+
+    {
+        void Update();
+        void Draw(SpriteBatch spriteBatch, Vector2 location);
+    }
+}
