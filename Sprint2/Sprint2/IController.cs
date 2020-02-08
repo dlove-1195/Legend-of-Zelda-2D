0cb5e77a4d0b80d97f8484217ff2b1bdf0f08f99
dlove-1195 <60125456+dlove-1195@users.noreply.github.com>
2020-02-08 16:33:09 -0500

    upload sprint2

---
+++ Sprint2/Sprint2/IController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2
+{
+    public interface IController
+    {
+        Game1 Game { get; set; }
+        void Update();
+    }
+}
