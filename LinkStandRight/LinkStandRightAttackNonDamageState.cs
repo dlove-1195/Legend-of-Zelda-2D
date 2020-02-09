8b2e886272680d12c0ca712e383d1483a90267b2
YilaiYan <40044317+YilaiYan@users.noreply.github.com>
2020-02-09 17:27:22 -0500

    Add files via upload

---
+++ LinkStandRight/LinkStandRightAttackNonDamageState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint2.IPlayer;
+
+namespace Sprint2.IPlayerState
+{
+    class LinkStandRightAttackNonDamageState:IPlayerState
+    {
+        private Link link;
+        public LinkStandRightAttackNonDamageState(Link link)
+        {
+            link = new LinkStandRightAttackNonDamageSprite(texture);
+            this.link = link;
+        }
+        public void ChangeToRight()
+        {
+            //already right
+        }
+        public void ChangeToleft()
+        {
+            link.state = new LinkStandLeftAttackNonDamageState(link);
+        }
+        public void ChangeToUp()
+        {
+            link.state = new LinkStandUpAttackNonDamageState(link);
+        }
+        public void ChangeToDown()
+        {
+            link.state = new LinkStandDownAttackNonDamageState(link);
+        }
+        public void GetDamage()
+        {
+            link.state = new LinkStandRightAttackDamageState(link);
+        }
+        public void Attack()
+        {
+            //already attack
+        }
+        public void ChangeToWalk()
+        {
+            link.state = new LinkWalkRightAttackNonDamageState(link);
+        }
+        public void ChangeToStand()
+        {
+            //already stand
+        }
+    }
+}
---
+++ LinkStandUp/LinkStandUpNonAttackDamageState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint2.IPlayer;
+
+namespace Sprint2.IPlayerState
+{
+    class LinkStandUpNonAttackDamageState: IPlayerState
+    {
+        private Link link;
+        public LinkStandUpNonAttackDamageState(Link link)
+        {
+            link = new LinkStandUpNonAttackDamageSprite(texture);
+            this.link = link;
+        }
+        public void ChangeToRight()
+        {
+            link.state = new LinkStandRightNonAttackDamageState(link);
+        }
+        public void ChangeToleft()
+        {
+            link.state = new LinkStandLeftNonAttackDamageState(link);
+        }
+        public void ChangeToUp()
+        {
+            //already up
+        }
+        public void ChangeToDown()
+        {
+            link.state = new LinkStandDownNonAttackDamageState(link);
+        }
+        public void GetDamage()
+        {
+            //already damage
+        }
+        public void Attack()
+        {
+            link.state = new LinkStandUpAttackDamageState(link);
+        }
+        public void ChangeToWalk()
+        {
+            link.state = new LinkWalkUpNonAttackDamageState(link);
+        }
+        public void ChangeToStand()
+        {
+            //already stand
+        }
+    }
+}
