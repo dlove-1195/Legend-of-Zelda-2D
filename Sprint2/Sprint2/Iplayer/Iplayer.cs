d150bcee72ba8ea0fce1b63115be5df5df477aea
Yilai Yan <yan.1029@osu.edu>
2020-02-09 11:10:12 -0500

    iplayer

--- Sprint2/Sprint2/Iplayer/Iplayer.cs
+++ Sprint2/Sprint2/Iplayer/Iplayer.cs
@@ -1,1 +1,21 @@
-//interface
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2.Iplayer
+{
+    
+     interface Iplayer
+    {
+        Link();
+        //other player constructor
+        void ChangeToRight();
+        void ChangeToleft();
+        void ChangeToUp();
+        void ChangeToDown();
+        void GetDamage();
+
+    }
+}
