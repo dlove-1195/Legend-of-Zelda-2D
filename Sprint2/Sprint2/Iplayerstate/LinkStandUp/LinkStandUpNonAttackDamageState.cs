cad089cb4d7de8b44a9d79ed2174283ec47d355f
YilaiYan <40044317+YilaiYan@users.noreply.github.com>
2020-02-09 16:15:35 -0500

    Add files via upload

---
+++ Sprint2/Sprint2/Iplayerstate/LinkStandUp/LinkStandUpNonAttackDamageState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprint2.IPlayer;
+
+namespace Sprint2.IplayerState
+{
+    class LinkStandUpNonAttackDamageState
+    {
+        private Link link;
+        public LinkStandUpNonAttackDamageState(Link link)
+        {
+            link = new LinkStandUpNonAttackDamageSprite(texture);
+            this.link = link;
+        }
+        public void ChangeToRight()
+        {
+            link.state = new LinkStandRightNonAttackDamageState(link);
+        }
+        public void ChangeToleft()
+        {
+            link.state = new LinkStandLeftNonAttackDamageState(link);
+        }
+        public void ChangeToUp()
+        {
+            //already up
+        }
+        public void ChangeToDown()
+        {
+            link.state = new LinkStandDownNonAttackDamageState(link);
+        }
+        public void GetDamage()
+        {
+            //already damage
+        }
+        public void Attack()
+        {
+            link.state = new LinkStandUpAttackDamageState(link);
+        }
+    }
+}
