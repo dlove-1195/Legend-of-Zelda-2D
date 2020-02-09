e40c0ef7e4aeb8410385ab823a7dd9fa246bb26a
Wei-prog <zong.73@osu.edu>
2020-02-09 13:18:49 -0500

    add texture storage cannot open project for some  reason

---
+++ Sprint2/Sprint2/Texture2DStorage.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Sprint2
+{
+  public static class Texture2DStorage
+		{
+			// Note that we are not using Game1's ContentLoader here (outside the scope of class methods) since it has not been instantiated yet
+			private static Texture2D enemySpriteSheet;
+			private static Texture2D itemSpriteSheet;
+		private static Texture2D linkSpriteSheet;
+
+		//initialize the Texture2D fields
+		  public static void LoadAllTextures(ContentManager content)
+			{
+
+			linkSpriteSheet = content.Load<Texture2D>("link");
+			enemySpriteSheet = content.Load<Texture2D>("enemy");
+			itemSpriteSheet = content.Load<Texture2D>("item");
+		}
+
+		   public static void UnloadAllTextures()
+			{
+			// unload all the Texture2Ds - not needed for the scope of this project
+		
+			}
+
+			public static Texture2D GetEnemySpriteSheet()
+			{
+				return enemySpriteSheet;
+			}
+
+			public static Texture2D GetItemSpriteSheet()
+			{
+				return itemSpriteSheet;
+			}
+
+		    public static Texture2D GetLinkSpriteSheet()
+		    {
+			return linkSpriteSheet;
+		    }
+
+	}
+	
+}
