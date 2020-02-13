d2eb4ce4090bd3e90d61fd0f53d76529ca96ac85
dlove-1195 <60125456+dlove-1195@users.noreply.github.com>
2020-02-13 15:34:34 -0500

    delete comment/add window dimension

---
+++ Sprint2/Game1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint2
+{
+   
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public Iplayer Link;
+        IController controller;
+        public static int WindowWidth;
+        public static int WindowHeight;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+
+            controller = new KeyboardController(this);
+            this.IsMouseVisible = true;
+            WindowWidth = graphics.PreferredBackBufferWidth;
+            WindowHeight = graphics.PreferredBackBufferHeight;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Texture2DStorage.LoadAllTextures(Content);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Link = new Link();
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+             
+            Link.Update();
+            controller.Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            Link.Draw(spriteBatch);
+                
+            
+            
+            base.Draw(gameTime);
+        }
+    }
+}
