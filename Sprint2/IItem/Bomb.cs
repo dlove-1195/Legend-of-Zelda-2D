7d1d8adbe0f6940e4e3baece5b6f2917994e3fc1
YilaiYan <40044317+YilaiYan@users.noreply.github.com>
2020-02-14 16:59:22 -0500

    Add files via upload

---
+++ Sprint2/IItem/Bomb.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace Sprint2
+{
+    class Bomb : Iitem
+    {
+        public Iitemstate state;
+        public ISprite bombSprite;
+        //initial position closed to link
+        public int posX;
+        public int posY;
+
+        public void explore()
+        {
+            //special method for class Bomb
+        }
+        public void Appear()
+        {
+            
+        }
+
+        public void Disappear()
+        {
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            bombSprite.Draw(spriteBatch, new Vector2(posX, posY));
+        }
+
+        public void goDown()
+        {
+            
+        }
+
+        public void goLeft()
+        {
+            
+        }
+
+        public void goRight()
+        {
+            
+        }
+
+        public void goUp()
+        {
+            
+        }
+
+        public void Update()
+        {
+            bombSprite.Update();
+        }
+    }
+}
