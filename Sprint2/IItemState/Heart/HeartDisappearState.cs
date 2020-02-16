46259c89ade61626f01dad80d7b27c42fc16010c
Wei-prog <59710517+Wei-prog@users.noreply.github.com>
2020-02-16 14:42:42 -0500

    Update HeartDisappearState.cs

--- Sprint2/IItemState/Heart/HeartDisappearState.cs
+++ Sprint2/IItemState/Heart/HeartDisappearState.cs
@@ -1,21 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Sprint2
 {
     class HeartDisappearState : Iitemstate
     {
+        private Heart heart;
+        public HeartDisappearState(Heart heart)
+        {
+            this.heart = heart;
+            heart.HeartSprite = new ItemHeartSprite();
+        }
         public void ChangeToAppear()
         {
-            
+            heart.state = new HeartAppearState(heart);
         }
 
         public void ChangeToDisappear()
         {
-           
+            //already disappear, do nothing
         }
     }
 }
