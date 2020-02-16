92bc179bc83009d224e5906a5862fe2cbbe69026
Wei-prog <59710517+Wei-prog@users.noreply.github.com>
2020-02-16 14:42:15 -0500

    Update HeartAppearState.cs

--- Sprint2/IItemState/Heart/HeartAppearState.cs
+++ Sprint2/IItemState/Heart/HeartAppearState.cs
@@ -1,12 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace Sprint2.IItemState
+namespace Sprint2
 {
-    class HeartAppearState
+    class HeartAppearState : Iitemstate
     {
+        private Heart heart;
+        private Texture2D texture = Texture2DStorage.GetItemSpriteSheet();
+
+        public HeartAppearState(Heart heart)
+        {
+            this.heart = heart;
+            heart.heartSprite = new ItemHeartSprite(texture);
+        }
+        public void ChangeToAppear()
+        {
+            //already appear, do nothing
+        }
+
+        public void ChangeToDisappear()
+        {
+            heart.state = new HeartDisappearState(heart);
+        }
     }
 }
+
