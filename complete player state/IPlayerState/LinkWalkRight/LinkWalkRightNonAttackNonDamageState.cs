5d546af579162c0f18d03c71ec94b54b51c2e38f
XinyiChen7787 <60306802+XinyiChen7787@users.noreply.github.com>
2020-02-16 13:21:28 -0500

    Update LinkWalkRightNonAttackNonDamageState.cs

--- complete player state/IPlayerState/LinkWalkRight/LinkWalkRightNonAttackNonDamageState.cs
+++ complete player state/IPlayerState/LinkWalkRight/LinkWalkRightNonAttackNonDamageState.cs
@@ -9,7 +9,7 @@ namespace Sprint2
     class LinkWalkRightNonAttackNonDamageState: Iplayerstate
     {
         private Link linkPlayer; 
-
+private Texture2D texture = Texture2DStorage.GetLinkSpriteSheet();
         //non damaged, non attack
         public LinkWalkRightNonAttackNonDamageState(Link link )
         {
