50551ef09154a5a460a3b847ddff5c1bdd5082b7
Tina Liang <liang.980@osu.edu>
2020-02-29 15:19:47 -0500

    add bow sprite class

---
+++ Sprint2/ISprite/ItemSprite/BowLeft.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprint2
+{
+    public class BowLeft: ISprite
+    {
+        public Texture2D Texture { get; set; }
+        private int posX = 71;
+        private int posY = 2;
+        private int width = 8;
+        private int height = 16;
+
+
+        public BowLeft(Texture2D texture)
+        {
+            Texture = texture;
+        }
+        public BowLeft()
+        {
+            //do nothing
+        }
+
+        public void Update()
+        {
+            //static bow
+            
+
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 vector)
+        {
+            if(Texture != null){
+                Rectangle sourceRectangle = new Rectangle(posX, posY, width, height);     
+                Rectangle destinationRectangle = new Rectangle((int)vector.X , (int)vector.Y, width * 3, height * 3);    // determine location and demension of the current frame
+
+                spriteBatch.Begin();
+                spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+                spriteBatch.End();
+            }
+        }
+
+    }
+}
---
+++ Sprint2/ISprite/ItemSprite/BowRight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprint2
+{
+    public class BowRight: ISprite
+    {
+        public Texture2D Texture { get; set; }
+        private int posX = 39;
+        private int posY = 12;
+        private int width = 8;
+        private int height = 16;
+
+
+        public BowRight(Texture2D texture)
+        {
+            Texture = texture;
+        }
+        public BowRight()
+        {
+            //do nothing
+        }
+
+        public void Update()
+        {
+            //static bow
+            
+
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 vector)
+        {
+            if(Texture != null){
+                Rectangle sourceRectangle = new Rectangle(posX, posY, width, height);     
+                Rectangle destinationRectangle = new Rectangle((int)vector.X , (int)vector.Y, width * 3, height * 3);    // determine location and demension of the current frame
+
+                spriteBatch.Begin();
+                spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+                spriteBatch.End();
+            }
+        }
+
+    }
+}
