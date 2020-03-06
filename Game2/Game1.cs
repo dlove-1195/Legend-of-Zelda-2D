0569eeace403a1d9b9482cd2b7b6451a68ed810b
zhinv <50930575+zhinv@users.noreply.github.com>
2020-03-06 12:21:06 -0500

    fix some problems in game class

--- Game2/Game1.cs
+++ Game2/Game1.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Xml;
-
+ 
 namespace Sprint2
 {
 
@@ -15,36 +13,32 @@ namespace Sprint2
         public Iplayer player;
         public Iitem item;
         public Inpc npcs;
-<<<<<<< HEAD
+ 
         public IRoom room;
-=======
-        public ICollisionDetection detection;
->>>>>>> 3fa391ec45cc3c48f9c367ddf57f5ef343d42078
+ 
+        public ICollisionDetection linkDetection;
+        public ICollisionDetection enemyDetection;
 
 
-        IController controller;
+        IController keyboardController;
         public static int WindowWidth=800;
         public static int WindowHeight=600;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-<<<<<<< HEAD
-            
-            graphics.PreferredBackBufferWidth = 1200;
-            graphics.PreferredBackBufferHeight = 1000;
-=======
+ 
            // graphics.IsFullScreen = false;
              graphics.PreferredBackBufferWidth = WindowWidth;
              graphics.PreferredBackBufferHeight = WindowHeight;
             // graphics.ApplyChanges();
->>>>>>> 3fa391ec45cc3c48f9c367ddf57f5ef343d42078
+ 
         }
 
         protected override void Initialize()
         {
 
-            controller = new KeyboardController(this);
+            keyboardController = new KeyboardController(this);
             this.IsMouseVisible = true;
             
 
@@ -57,23 +51,16 @@ namespace Sprint2
             Texture2DStorage.LoadAllTextures(Content);
             spriteBatch = new SpriteBatch(GraphicsDevice);
             player = new Link();
-            enemy = new  Keese();
-            item = new Heart();
-            npcs = new  Princess();
+           
             room = new Room1();
+            linkDetection = new LinkCollisionDetection(room, player);
+            enemyDetection = new EnemyCollisionDetection(room);
 
-<<<<<<< HEAD
+            //ROOM load in camera controller ??  no
+            //load original first room in the game class and change room in the controller
+            //same as linkDetection and enemyDectection 
             
-=======
-            //ROOM load in camera controller ?? 
-            //detection = new LinkCollisionDetection(room, player);
 
-
-
-
-
-
->>>>>>> 3fa391ec45cc3c48f9c367ddf57f5ef343d42078
         }
 
         protected override void UnloadContent()
@@ -84,21 +71,14 @@ namespace Sprint2
         protected override void Update(GameTime gameTime)
         {
 
-            player.Update();
-            enemy.Update();
-            item.Update();
-            npcs.Update();
-<<<<<<< HEAD
-            controller.Update();
+            player.Update(); 
+            //will have camera and mouse controller in the future 
+            keyboardController.Update();
+            linkDetection.Update();
+            enemyDetection.Update();
             room.Update();
-=======
-
-           // detection.Update();
-
-            controller.Update();
-
-
->>>>>>> 3fa391ec45cc3c48f9c367ddf57f5ef343d42078
+            
+ 
             base.Update(gameTime);
         }
 
@@ -107,12 +87,10 @@ namespace Sprint2
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
             player.Draw(spriteBatch);
-
-            enemy.Draw(spriteBatch);
-            item.Draw(spriteBatch);
-            npcs.Draw(spriteBatch);
-            spriteBatch.End();
             room.Draw(spriteBatch);
+ 
+            spriteBatch.End();
+           
             base.Draw(gameTime);
         }
     }
