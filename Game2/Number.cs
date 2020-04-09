ddaa307b1e695f814fb45436af7b5dd667dc72ff
Wei-prog <59710517+Wei-prog@users.noreply.github.com>
2020-04-09 01:13:46 -0400

    Add files via upload

---
+++ Game2/Number.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+
+namespace Sprint2
+{
+    class Number
+    {
+        private Texture2D numberTexture = Texture2DStorage.GetNumberSpriteSheet();
+        private Dictionary<int, Vector2> numMap = new Dictionary<int, Vector2>(){
+            {0, new Vector2 (19,19)},
+            {1, new Vector2 (34,19)},
+            { 2,new Vector2 (50,19)},
+            { 3,new Vector2 (66,19)},
+            { 4,new Vector2 (82,19)},
+            { 5,new Vector2 (96,19)},
+            { 6,new Vector2 (110,19)},
+            { 7,new Vector2 (125,19)},
+            { 8,new Vector2 (141,19)},
+            { 9,new Vector2 (156,19)}
+            };
+        private Vector2 drawLoc;
+        private Vector2 numSize = new Vector2(13, 13);
+        private Vector2 drawSize = new Vector2(25,25);
+        private int num;
+
+        public Number(int number, Vector2 vector)
+        {
+            num = number;
+            drawLoc = vector;
+        }
+
+        public void increment() { num++; }
+        public void decrement() { num--; }
+
+        public void Update() { 
+        
+        }
+        //largest number 99;
+        public void Draw(SpriteBatch spriteBatch) {
+            if (num < 10)
+            {
+                Rectangle sourceRectangle = new Rectangle((int)numMap[num].X, (int)numMap[num].Y, (int)numSize.X, (int)numSize.Y);
+                Rectangle destinationRectangle = new Rectangle((int)drawLoc.X, (int)drawLoc.Y, (int)drawSize.X, (int)drawSize.Y);
+                spriteBatch.Draw(numberTexture, destinationRectangle, sourceRectangle, Color.White);
+            }
+            else {
+                int num2 = num % 10;
+                int num1 = num / 10;
+                Rectangle sourceRectangle1 = new Rectangle((int)numMap[num1].X, (int)numMap[num1].Y, (int)numSize.X, (int)numSize.Y);
+                Rectangle destinationRectangle1 = new Rectangle((int)drawLoc.X, (int)drawLoc.Y, (int)drawSize.X, (int)drawSize.Y);
+
+                spriteBatch.Draw(numberTexture, destinationRectangle1, sourceRectangle1, Color.White);
+
+                Rectangle sourceRectangle2 = new Rectangle((int)numMap[num2].X, (int)numMap[num2].Y, (int)numSize.X, (int)numSize.Y);
+                Rectangle destinationRectangle2 = new Rectangle((int)drawLoc.X+ (int)drawSize.X, (int)drawLoc.Y, (int)drawSize.X, (int)drawSize.Y);
+
+                spriteBatch.Draw(numberTexture, destinationRectangle2, sourceRectangle2, Color.White);
+
+            }
+        }
+
+    }
+}
