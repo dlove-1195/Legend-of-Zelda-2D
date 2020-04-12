492b5d98548260fab3605a9c6cf528125f20bc1c
Tina Liang <liang.980@osu.edu>
2020-04-12 16:23:46 -0400

    letter winstate done

--- Game2/GameState/WinState.cs
+++ Game2/GameState/WinState.cs
@@ -8,18 +8,22 @@ namespace Sprint2
     public class WinState : IGameState
     {
 
-        private Texture2D ButtonTexture = Texture2DStorage.GetButtonSpriteSheet();
+        //private Texture2D ButtonTexture = Texture2DStorage.GetButtonSpriteSheet();
         private Texture2D WinTriTexture = Texture2DStorage.GetTriForceSpriteSheet();
         private Texture2D BackgroundTexture = Texture2DStorage.GetBlackBackgroundSpriteSheet();
+        private string sentence1 = "PRESS S TO RESTART GAME";
+        private string sentence2 = "PRESS ESCAPE TO QUIT GAME";
+        private string win = "YOU WIN";
 
-        private Vector2 button1Location = new Vector2(200, 260);
-        private Vector2 button2Location = new Vector2(500, 260);
+        private Vector2 winLocation = new Vector2(300,350);
+        private Vector2 sentence1Location = new Vector2(150, 390);
+        private Vector2 sentence2Location = new Vector2(150, 430);
+        private Vector2 fontSize = new Vector2(20,20);
         private int width = 140;
         private int height = 70;
         private int change = 0;
         private Vector2 triLoc = new Vector2(250, -70);
         private Game1 myGame;
-        //add font "you win" later 
 
         public IInventory inventoryBar { get; set; }
         private IController winStateController;
@@ -67,7 +71,7 @@ namespace Sprint2
 
             if (change >= 400)
             {
-
+                /*
                 //Restart button
                 Rectangle sourceButtonRectangle1 = new Rectangle(478, 42, 92, 34);
                 Rectangle destinationButtonRectangle1 = new Rectangle((int)button1Location.X, (int)button1Location.Y, 100, 55);
@@ -77,7 +81,11 @@ namespace Sprint2
                 Rectangle sourceButtonRectangle2 = new Rectangle(477, 263, 92, 34);
                 Rectangle destinationButtonRectangle2 = new Rectangle((int)button2Location.X, (int)button2Location.Y, 100, 55);
                 spriteBatch.Draw(ButtonTexture, destinationButtonRectangle2, sourceButtonRectangle2, Color.White);
+                */
 
+                LetterGenerator.drawSentence(spriteBatch, win, winLocation, fontSize);
+                LetterGenerator.drawSentence(spriteBatch, sentence1, sentence1Location, fontSize);
+                LetterGenerator.drawSentence(spriteBatch, sentence2, sentence2Location, fontSize);
 
 
                 //tri logo 
