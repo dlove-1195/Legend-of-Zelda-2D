04d01f39d8d5be12a6641a1285edf04b5d669274
zhinv <song.1500@osu.edu>
2020-04-24 00:35:41 -0400

    add push box feature(unfinished)

---
+++ Game2/IItem/StaticItem/box.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace Sprint2
+{
+    public class Box : IItem
+    {
+        //this part only needed for link item
+        public int Count { get; set; } = 0;
+        public int TotalCount { get; set; } = 100;
+        public bool Appear { get; set; } = true;
+        private int p = 100;
+        //Sprite parameters
+        private Texture2D texture = Texture2DStorage.GetDoorSpriteSheet();
+        private int sourceLocX;
+        private int sourceLocY;
+        private int width ;
+        private int height;
+
+        private int desWidth=48;
+        private int desHeight=48;
+        private string Direction { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        private int originX;
+        private int originY;
+        public Rectangle BoundingBox { get; set; }  
+        
+
+        public Box(string direction, Vector2 desLoc)
+        {
+            this.Direction = direction;
+            PosX = (int)desLoc.X;
+            PosY = (int)desLoc.Y;
+            originX = (int)desLoc.X;
+            originY = (int)desLoc.Y;
+            BoundingBox = new Rectangle(PosX, PosY, desWidth, desHeight);
+
+
+        }
+
+        public void Update()
+        {
+            BoundingBox = new Rectangle(PosX, PosY, desWidth, desHeight);
+             
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+             
+            Rectangle sourceRectangle = new Rectangle(sourceLocX, sourceLocY, width, height);
+            Rectangle destinationRectangle = new Rectangle(PosX, PosY, desWidth, desHeight);
+            spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+
+        //method needed for non static item
+        //here to implement the interface
+        public int GetItem()
+        {
+            return p;
+        }
+        public static void changeState()
+        {
+            //do nothing
+        }
+
+        public void ChangeSprite(ISprite sprite)
+        {
+            //do nothing
+        }
+
+    }
+}
